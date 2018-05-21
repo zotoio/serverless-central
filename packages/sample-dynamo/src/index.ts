9d4892dcdb050a5f19ba1bf573e95ab9f9cf065b
wyvern8 <wyvern@smartcreations.com.au>
2018-05-21 19:34:00 +1000

    add dynamo offline per generator updates

--- packages/sample-dynamo/src/index.ts
+++ packages/sample-dynamo/src/index.ts
@@ -8,15 +8,21 @@ interface Response {
     body: string;
 }
 
+/**
+ * this sample handler inserts current request headers to ddb, and also queries for any items with passed in id
+ * /req/{id}
+ *
+ * @param event
+ * @param {Context} context
+ * @param {Callback} callback
+ * @returns {Promise<void>}
+ */
+
 const handler: Handler = async (event: any, context: Context, callback: Callback) => {
     const id = event.pathParameters.id;
     console.log(`processing request for id: ${id}`);
 
-    const dynamo = new AWS.DynamoDB();
-    const docClient = new AWS.DynamoDB.DocumentClient({
-        convertEmptyValues: true,
-        service: dynamo
-    });
+    const docClient = await getDocClient();
 
     const response: Response = {
         statusCode: 200,
@@ -38,7 +44,7 @@ const handler: Handler = async (event: any, context: Context, callback: Callback
             () => {
                 console.log(`PutItem succeeded:  ${event.requestContext.requestId}`);
             },
-            err => {
+            (err: any) => {
                 console.log(`Unable to Put item. Error: ${JSON.stringify(err)}`);
             }
         );
@@ -58,24 +64,47 @@ const handler: Handler = async (event: any, context: Context, callback: Callback
         .query(queryParams)
         .promise()
         .then(
-            data => {
+            (data: any) => {
                 console.log('Query succeeded.');
                 const results: any = [];
                 if (data.Items) {
-                    data.Items.forEach(item => {
+                    data.Items.forEach((item: any) => {
                         console.log(item);
                         results.push(item);
                     });
                 }
                 response.body = JSON.stringify(results);
             },
-            err => {
+            (err: any) => {
                 response.body = `Unable to query. Error: ${JSON.stringify(err)}`;
                 console.error(response.body);
             }
         );
 
     callback(undefined, response);
+};
+
+const getDocClient: any = async () => {
+    const ddbConfig = process.env.IS_OFFLINE
+        ? {
+              region: 'localhost',
+              endpoint: 'http://localhost:8000'
+          }
+        : {};
+
+    const dynamo = new AWS.DynamoDB(ddbConfig);
+
+    const ddbDocClientConfig = {
+        convertEmptyValues: true,
+        service: dynamo
+    };
+
+    if (process.env.IS_OFFLINE) {
+        ddbDocClientConfig['region'] = 'localhost';
+        ddbDocClientConfig['endpoint'] = 'http://localhost:8000';
+    }
+
+    return new AWS.DynamoDB.DocumentClient(ddbDocClientConfig);
 };
 
 export { handler };
